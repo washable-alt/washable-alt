67d4845d8fffa9d66185123510990d2109b54247
Melvin <melvinhjw@gmail.com>
2023-08-19 18:53:59 +0800

    colorgram package extract rgb and print a list of tuples

---
+++ Day18/main.py
@@ -0,0 +1,20 @@
+import colorgram
+
+
+rgb_colors = []
+
+#extract to 30 colors
+colors = colorgram.extract(r'filepath', 30)
+
+print(colors)
+
+# learn how to extract from a tuple and put r, g, b colors to a list
+for color in colors:
+    print(color.rgb)
+    
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r,g,b)
+    rgb_colors.append(new_color)
+print(rgb_colors)
